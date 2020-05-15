3347ff69cd0890a7ff32bd00f72ae64cb743a10d
Nakamura, Yuuichi <Yuuichi.A.Nakamura@sony.com>
2020-05-15 16:54:39 +0900

    smp

--- hw/arm/cxd56.c
+++ hw/arm/cxd56.c
@@ -146,8 +146,13 @@ static void cxd56_crg_write(void *opaque, hwaddr offset,
     case 0x0030:
         fprintf(stderr, "CRG: reset 0x%x\n", (int)value);
         if (value != 0) {
-            fprintf(stderr, "CRG: boot cpu 1\n");
-            arm_set_cpu_on_and_reset(1);
+            int cpu;
+            for (cpu = 1; cpu < 6; cpu++) {
+                if (value & (1 << (16 + cpu))) {
+                    fprintf(stderr, "CRG: boot cpu %d\n", cpu);
+                    arm_set_cpu_on_and_reset(cpu);
+                }
+            }
         }
 
         return;
@@ -196,11 +201,7 @@ static void cxd56_swint_write(void *opaque, hwaddr offset,
                            uint64_t value, unsigned size)
 {
     int cpu = (offset / 4) - 2;
-    if (value) {
-        qemu_irq_raise(cxd56_swint[cpu]);
-    } else {
-        qemu_irq_lower(cxd56_swint[cpu]);
-    }
+    qemu_set_irq(cxd56_swint[cpu], value);
 #if 0
     fprintf(stderr,
                   "swint: write at bad offset 0x%x 0x%x\n", (int)offset, (int)value);
@@ -209,6 +210,60 @@ static void cxd56_swint_write(void *opaque, hwaddr offset,
 
 static const MemoryRegionOps cxd56_swint_ops = {
     .write = cxd56_swint_write,
+    .endianness = DEVICE_NATIVE_ENDIAN,
+};
+
+/***************/
+
+typedef struct {
+    MemoryRegion iomem;
+} cxd56_nvic_sysreg_state;
+
+static MemoryRegion *cxd56_nvic_sysreg[8];
+
+static MemTxResult cxd56_nvic_sysreg_write(void *opaque, hwaddr addr,
+                                    uint64_t value, unsigned size,
+                                    MemTxAttrs attrs)
+{
+    return memory_region_dispatch_write(cxd56_nvic_sysreg[current_cpu->cpu_index], addr, value, size_memop(size) | MO_TE, attrs);
+}
+
+static MemTxResult cxd56_nvic_sysreg_read(void *opaque, hwaddr addr,
+                                   uint64_t *data, unsigned size,
+                                   MemTxAttrs attrs)
+{
+    return memory_region_dispatch_read(cxd56_nvic_sysreg[current_cpu->cpu_index], addr, data, size_memop(size) | MO_TE, attrs);
+}
+
+static const MemoryRegionOps cxd56_nvic_sysreg_ops = {
+    .read_with_attrs = cxd56_nvic_sysreg_read,
+    .write_with_attrs = cxd56_nvic_sysreg_write,
+    .endianness = DEVICE_NATIVE_ENDIAN,
+};
+
+typedef struct {
+    MemoryRegion iomem;
+} cxd56_nvic_systick_state;
+
+static MemoryRegion *cxd56_nvic_systick[8];
+
+static MemTxResult cxd56_nvic_systick_write(void *opaque, hwaddr addr,
+                                    uint64_t value, unsigned size,
+                                    MemTxAttrs attrs)
+{
+    return memory_region_dispatch_write(cxd56_nvic_systick[current_cpu->cpu_index], addr, value, size_memop(size) | MO_TE, attrs);
+}
+
+static MemTxResult cxd56_nvic_systick_read(void *opaque, hwaddr addr,
+                                   uint64_t *data, unsigned size,
+                                   MemTxAttrs attrs)
+{
+    return memory_region_dispatch_read(cxd56_nvic_systick[current_cpu->cpu_index], addr, data, size_memop(size) | MO_TE, attrs);
+}
+
+static const MemoryRegionOps cxd56_nvic_systick_ops = {
+    .read_with_attrs = cxd56_nvic_systick_read,
+    .write_with_attrs = cxd56_nvic_systick_write,
     .endianness = DEVICE_NATIVE_ENDIAN,
 };
 
@@ -232,6 +287,8 @@ static void cxd56_devices(void)
     cxd56_crg_state *crg;
     cxd56_cpuid_state *cpuid;
     cxd56_swint_state *swint;
+    cxd56_nvic_sysreg_state *nvic_sysreg;
+    cxd56_nvic_systick_state *nvic_systick;
 
     topreg = g_new0(cxd56_topreg_state, 1);
     memory_region_init_io(&topreg->iomem, NULL, &cxd56_topreg_ops, topreg, "topreg", 0x3000);
@@ -255,6 +312,13 @@ static void cxd56_devices(void)
     swint = g_new0(cxd56_swint_state, 1);
     memory_region_init_io(&swint->iomem, NULL, &cxd56_swint_ops, swint, "swint", 0x1000);
     memory_region_add_subregion(get_system_memory(), 0x4600c000, &swint->iomem);
+
+    nvic_sysreg = g_new0(cxd56_nvic_sysreg_state, 1);
+    memory_region_init_io(&nvic_sysreg->iomem, NULL, &cxd56_nvic_sysreg_ops, nvic_sysreg, "nvic_sysreg", 0x1000);
+    memory_region_add_subregion(get_system_memory(), 0xe000e000, &nvic_sysreg->iomem);
+    nvic_systick = g_new0(cxd56_nvic_systick_state, 1);
+    memory_region_init_io(&nvic_systick->iomem, NULL, &cxd56_nvic_systick_ops, nvic_systick, "nvic_systick", 0xe0);
+    memory_region_add_subregion_overlap(get_system_memory(), 0xe000e010, &nvic_systick->iomem, 1);
 #if 0
     /* Add dummy regions for the devices we don't implement yet,
      * so guest accesses don't cause unlogged crashes.
@@ -283,9 +347,12 @@ static void cxd56_init(MachineState *ms)
     /* Tiny boot loader */
     static uint16_t code[] = {
         0x0000, 0x0d00,         /* vector: initial SP */
-        0x0009, 0x0000,         /* vector: initial PC */
+        0x000d, 0x0000,         /* vector: initial PC */
+        0xed08, 0xe000,         /* (NVIC_VECTAB) */
         0x2000,                 /* movs   r0, #0 */
         0x6801,                 /* ldr    r1, [r0, #0] */
+        0x6880,                 /* ldr    r0, [r0, #8] */
+        0x6001,                 /* str    r1, [r0, #0] */
         0x6808,                 /* ldr    r0, [r1, #0] */
         0x4685,                 /* mov    sp, r0 */
         0x6848,                 /* ldr    r0, [r1, #4] */
@@ -298,43 +365,23 @@ static void cxd56_init(MachineState *ms)
     memory_region_add_subregion(system_memory, 0x0d000000, sram);
 
     for (n = 0; n < smp_cpus; n++) {
-#if 0
-        Object *cpuobj = object_new(ms->cpu_type);
-        object_property_set_link(cpuobj, OBJECT(system_memory), "memory",
-                                &error_abort);
-        object_property_set_bool(cpuobj, true, "realized", &error_abort);
-
-
-#if 0
-#define NAME_SIZE 20
-        char name[NAME_SIZE];
-        snprintf(name, NAME_SIZE, "cpu%d", i);
-        object_initialize_child(obj, name, &s->cpu[i], sizeof(s->cpu[i]),
-                                ARM_CPU_TYPE_NAME("cortex-a9"),
-                                &error_abort, NULL);
-//
-        Object *cpuobj = object_new(ms->cpu_type);
-        object_property_set_bool(cpuobj, true, "realized", &error_fatal);
-//        cpu_irq[n] = qdev_get_gpio_in(DEVICE(cpuobj), ARM_CPU_IRQ);
-#endif
-#else
         nvic = qdev_create(NULL, TYPE_ARMV7M);
-        qdev_prop_set_uint32(nvic, "cpunum", n);
         qdev_prop_set_uint32(nvic, "num-irq", NUM_IRQ_LINES);
         qdev_prop_set_string(nvic, "cpu-type", ms->cpu_type);
         qdev_prop_set_bit(nvic, "enable-bitband", false);
-        object_property_set_link(OBJECT(nvic), OBJECT(get_system_memory()),
-                                         "memory", &error_abort);
         if (n > 0) {
             object_property_set_bool(OBJECT(nvic), true,
                                      "start-powered-off", &error_abort);
         }
 
         qdev_init_nofail(nvic);
+
+        ARMv7MState *s = ARMV7M(nvic);
+        cxd56_nvic_sysreg[n] = &s->nvic.sysregmem;
+        cxd56_nvic_systick[n] = &s->nvic.systickmem;
 #if 0
         qdev_connect_gpio_out_named(nvic, "SYSRESETREQ", 0,
                                     qemu_allocate_irq(&do_sys_reset, NULL, 0));
-#endif
 #endif
         cxd56_swint[n] = qdev_get_gpio_in(nvic, 96);
         if (n == 0) {
@@ -342,7 +389,6 @@ static void cxd56_init(MachineState *ms)
         }
     }
 
-//    pl011_create(0x041ac000, qdev_get_gpio_in(nvic, 11), serial_hd(0));
     cxd56_devices();
 
     system_clock_scale = NANOSECONDS_PER_SECOND / 160000000;
